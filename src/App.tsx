81502c91c5f915ce4aa29b5d5b1ef55e07668a97
Anton Sidorov <cohojet@gmail.com>
2022-08-12 19:42:43 +0300

    Add background prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import './css/particles.css'
+
 function App() {
+  var backgroundColor = '#7F006E'
+  var particleColor = '#FF00DC'
+
+  let particles : JSX.Element[] = [];
+  for (var i = 0; i < 1000; i++) {
+    particles.push(
+        <div className='circle-container' key={i}>
+            <div className='circle' style={{ backgroundImage: 'radial-gradient(' + particleColor + ', ' + particleColor + ' 10%, ' + particleColor + '00 56%)' }}/>
+        </div>
+    );
+  }
+  
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="container" style={{ backgroundColor: backgroundColor }}>
+      <img className='background' src={require('./images/background.png')}/>
+      {particles}
     </div>
   );
 }

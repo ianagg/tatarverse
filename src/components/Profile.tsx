eb4a7057666160fe85f7337f68c6f506f6bf7d9f
Iana <himishoichi@gmail.com>
2022-08-12 22:34:14 +0300

    Add profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,7 +2,23 @@ import '../styles/Profile.css';
 import { Character } from './Character';
 
 function Profile(char: Character) {
-  return <div className="prof">Profile</div>;
+  return (
+    <div className="prof">
+      <h1>{char.name}</h1>
+      <h3>Озвучили: {char.voice}</h3>
+      <div className="table">
+        <label>День рождения:</label>
+        <span>{char.birthDate}</span>
+        <label>Группа:</label>
+        <span>{char.group}</span>
+        <label>Глаз Бога:</label>
+        <span>{char.vision}</span>
+        <label>Созвездие:</label>
+        <span>{char.constellation}</span>
+      </div>
+      {char.description}
+    </div>
+  );
 }
 
 export default Profile;

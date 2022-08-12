23c599881f61e806d63b16ae7415fdcb279e4a11
Iana <himishoichi@gmail.com>
2022-08-12 23:02:32 +0300

    Fix menu

---
+++ src/components/Сonstellation.tsx
@@ -0,0 +1,8 @@
+import '../styles/Сonstellation.css';
+import { Character } from './Character';
+
+function Сonstellation(char: Character) {
+  return <div className="con">Сonstellation</div>;
+}
+
+export default Сonstellation;
